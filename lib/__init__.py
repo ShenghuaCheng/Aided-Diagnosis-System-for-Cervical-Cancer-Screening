a8ce5eaa05d2f4c85bbbdbe60f9a0c62200c379e
LiamTTT <liam.liusibo@gmail.com>
2021-05-31 17:25:17 +0800

    Revert "finish module SlideReader source codes."
    This reverts commit 37a26a2fdb4e7c69be796a18ffb1a1360a89e394.

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,8 +1,1 @@
 # -*- coding:utf-8 -*-
-import platform
-from .sdpcreader.sdpc_reader import Sdpc
-SYSTEM_TYPE = platform.system()
-if SYSTEM_TYPE == "Windows":
-    from .srpreader.srp_python_win.pysrp import Srp
-elif SYSTEM_TYPE == "Linux":
-    from .srpreader.srp_python_linux.pysrp import Srp
