da7a75cd6a4e9068ae182514b54873d94b0a5aa1
LiamTTT <liam.liusibo@gmail.com>
2021-11-09 20:27:52 +0800

    refactor augmentation module

---
+++ core/data/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+This file is a part of project "Aided-Diagnosis-System-for-Cervical-Cancer-Screening".
+See https://github.com/ShenghuaCheng/Aided-Diagnosis-System-for-Cervical-Cancer-Screening for more information.
+
+File name: __init__.py
+Description: 
+"""
+from .augmentations import *
