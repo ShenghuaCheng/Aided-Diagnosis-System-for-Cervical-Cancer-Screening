cf81420385d403bbbb1cf298aa7ce4efec010be1
LiamTTT <liam.liusibo@gmail.com>
2021-11-16 20:29:37 +0800

    finish baseclass of train configs and example for resnet related config.

---
+++ core/config/base_config.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+This file is a part of project "Aided-Diagnosis-System-for-Cervical-Cancer-Screening".
+See https://github.com/ShenghuaCheng/Aided-Diagnosis-System-for-Cervical-Cancer-Screening for more information.
+
+File name: base_config
+Description: abstract base class for all train config.
+"""
+
+import pprint
+from abc import ABCMeta, abstractmethod
+from tabulate import tabulate
+
+from keras.models import Model
+
+
+class BaseConfig(metaclass=ABCMeta):
+
+    def __init__(self):
+        self.output_dir = "./OUTPUTS"
+
+    @abstractmethod
+    def create_model(self, weight=None) -> Model:
+        raise NotImplementedError
+
+    @abstractmethod
+    @property
+    def optimizer(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    @property
+    def loss(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    @property
+    def metrics(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_lr_scheduler(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_callbacks(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_train_loader(self):
+        pass
+
+    @abstractmethod
+    def get_validate_loader(self):
+        pass
+
+    @abstractmethod
+    def get_test_loader(self):
+        pass
+
+    def __repr__(self):
+        table_header = ["keys", "values"]
+        exp_table = [
+            (str(k), pprint.pformat(v))
+            for k, v in vars(self).items()
+            if not k.startswith("_")
+        ]
+        return tabulate(exp_table, headers=table_header, tablefmt="fancy_grid")
