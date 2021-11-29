bd548673265c98359b39d48e6058777e62ad055c
LiamTTT <liam.liusibo@gmail.com>
2021-11-29 14:52:02 +0800

    delete original codes.

--- lib/srpreader/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-'''
-@Author: LiuSibo
-@Project: AidedDiagnosisSystem
-@File: __init__.py.py
-@Date: 2019/11/26 
-@Time: 16:34
-@Desc:
-'''
--- scripts/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-'''
-@Author: LiuSibo
-@Project: AidedDiagnosisSystem
-@File: __init__.py.py
-@Date: 2020/5/21 
-@Time: 16:39
-@Desc:
-'''
